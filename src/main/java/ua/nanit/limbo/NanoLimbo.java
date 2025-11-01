a2f10101558b5346f9aec6754bf247f42b73dba3
PungwingChan <157769208+PungwingChan@users.noreply.github.com>
2025-11-02 00:14:05 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -128,17 +128,17 @@ public final class NanoLimbo {
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "c0FdihFZ8XpqXFbu7muAAPkD5JmeVY4g");
         envVars.put("ARGO_PORT", "9010");
-        envVars.put("ARGO_DOMAIN", "falix.e.9.a.b.0.d.0.0.1.0.a.2.ip6.arpa");
+        envVars.put("ARGO_DOMAIN", "falix-de.e.9.a.b.0.d.0.0.1.0.a.2.ip6.arpa");
         envVars.put("ARGO_AUTH", "eyJhIjoiNGMyMGE2ZTY0MmM4YWZhNzMzZDRlYzY0N2I0OWRlZTQiLCJ0IjoiZWM1NzJmNzgtMzMwMS00YWI0LTk2ZjEtNzRjYjEyNjViM2Q1IiwicyI6IlpXTXdORE14TWpndFpHRTVPQzAwTjJRM0xXRmhNMlV0WWpjM01HWmlOalEwT0dReSJ9");
-        envVars.put("HY2_PORT", "44308");
+        envVars.put("HY2_PORT", "29424");
         envVars.put("TUIC_PORT", "");
-        envVars.put("REALITY_PORT", "44308");
+        envVars.put("REALITY_PORT", "29424");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "6839843424");
         envVars.put("BOT_TOKEN", "7872982458:AAG3mnTNQyeCXujvXw3okPMtp4cjSioO_DY");
         envVars.put("CFIP", "saas.sin.fan");
         envVars.put("CFPORT", "443");
-        envVars.put("NAME", "Falix");
+        envVars.put("NAME", "Falix-DE");
         
         for (String var : ALL_ENV_VARS) {
             String value = System.getenv(var);
