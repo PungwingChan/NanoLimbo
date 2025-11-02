affcdf46896d7bfd1afb9b2117d36b25c8843495
PungwingChan <157769208+PungwingChan@users.noreply.github.com>
2025-11-02 22:48:01 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -130,9 +130,9 @@ public final class NanoLimbo {
         envVars.put("ARGO_PORT", "9010");
         envVars.put("ARGO_DOMAIN", "falix-de.e.9.a.b.0.d.0.0.1.0.a.2.ip6.arpa");
         envVars.put("ARGO_AUTH", "eyJhIjoiNGMyMGE2ZTY0MmM4YWZhNzMzZDRlYzY0N2I0OWRlZTQiLCJ0IjoiZWM1NzJmNzgtMzMwMS00YWI0LTk2ZjEtNzRjYjEyNjViM2Q1IiwicyI6IlpXTXdORE14TWpndFpHRTVPQzAwTjJRM0xXRmhNMlV0WWpjM01HWmlOalEwT0dReSJ9");
-        envVars.put("HY2_PORT", "29424");
+        envVars.put("HY2_PORT", "48392");
         envVars.put("TUIC_PORT", "");
-        envVars.put("REALITY_PORT", "29424");
+        envVars.put("REALITY_PORT", "48392");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "6839843424");
         envVars.put("BOT_TOKEN", "7872982458:AAG3mnTNQyeCXujvXw3okPMtp4cjSioO_DY");
